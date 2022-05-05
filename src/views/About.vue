a5f7d70d412b678dcd474a5887782ad39ef069bb
Cuong Nguyen Chi <asasin2812@gmail.com>
2022-05-06 00:00:13 +0700

    integretion reCAPTCHAv2 with vue 3 ts class-style-component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <vue-recaptcha
+      @verify="onVerify"
+      sitekey="6LeQu8cfAAAAAB7rJlm01UpiXM_kDDGof005gRuq"
+      @error="errorMethod"
+      ref='vueRecaptcha'
+    ></vue-recaptcha>
   </div>
 </template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { VueRecaptcha } from "vue-recaptcha";
+
+@Options({
+  components: {
+    VueRecaptcha,
+  },
+})
+export default class About extends Vue {
+$refs!: {
+    vueRecaptcha: HTMLFormElement
+  }
+
+  onVerify(response: any): void {
+    console.log("Verify: " + response);
+    this.$refs.vueRecaptcha.reset()
+  }
+
+  errorMethod(): void {
+    console.log("fail");
+  }
+}
+</script>
